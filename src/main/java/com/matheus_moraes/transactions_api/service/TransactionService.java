aa63f463c0e32a7c9d4cc65324389086c85b6222
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-08-04 09:05:39 -0300

    add: value smaller than 0 validation

--- src/main/java/com/matheus_moraes/transactions_api/service/TransactionService.java
+++ src/main/java/com/matheus_moraes/transactions_api/service/TransactionService.java
@@ -17,11 +17,16 @@ public class TransactionService {
     private final List<TransactionReqDto> transactionsList = new ArrayList<>();
 
     private void validateNewTransaction(TransactionReqDto transaction, String processId){
-        log.info("[ID:{}] Service: start transaction validation", processId);
+        log.info("[ID: {}] Service: start transaction validation", processId);
         if(transaction.dataHora().isAfter(OffsetDateTime.now())){
             UnprocessableEntityException ex = new UnprocessableEntityException("Date/hour is in the future");
-            log.error("[ID:{}] Service: date/hour is in the future", processId, ex);
+            log.error("[ID: {}] Service: date/hour is in the future", processId, ex);
+            throw ex;
+        } else if (transaction.valor() < 0) {
+            UnprocessableEntityException ex = new UnprocessableEntityException("Value smaller than 0");
+            log.error("[ID: {}] Service: Value smaller than 0", processId, ex);
             throw ex;
         }
+        log.info("[ID: {}] Service: Transaction validation sucessful", processId);
     }
 }
