eeebc1b98ffa2d0812ee63703e73beb421375be6
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-08-04 00:20:13 -0300

    Create TransactionService.java

---
+++ src/main/java/com/matheus_moraes/transactions_api/service/TransactionService.java
@@ -0,0 +1,15 @@
+package com.matheus_moraes.transactions_api.service;
+
+import com.matheus_moraes.transactions_api.dto.req.TransactionReqDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TransactionService {
+
+    
+}
