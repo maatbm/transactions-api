d012d927a0e1b1b1ad64e0e6b2484e5d8d621d55
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-08-04 00:16:06 -0300

    Start project

---
+++ src/test/java/com/matheus_moraes/transactions_api/TransactionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus_moraes.transactions_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
