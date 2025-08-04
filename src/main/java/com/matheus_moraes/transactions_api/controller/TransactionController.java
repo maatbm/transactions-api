8a5c8db5674d116184c0fae41ea7757995725512
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-08-04 20:25:02 -0300

    add: delete all transactions controller

--- src/main/java/com/matheus_moraes/transactions_api/controller/TransactionController.java
+++ src/main/java/com/matheus_moraes/transactions_api/controller/TransactionController.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -23,10 +20,23 @@ public class TransactionController {
 
     @PostMapping
     public ResponseEntity<Void> addNewTransaction(@Valid @RequestBody TransactionReqDto request) {
-        String processId = UUID.randomUUID().toString();
+        String processId = this.generateprocessId();
         log.info("[ID: {}] Controller: Received request to add a new transaction -> {}", processId, request);
         transactionService.addNewTransaction(request, processId);
         log.info("[ID: {}] Controller: Transaction processed successfully. Returning HTTP status {}.", processId, HttpStatus.CREATED);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteAllTransactions() {
+        String processId = this.generateprocessId();
+        log.info("[ID: {}] Controller: Received request to delete all transactions.", processId);
+        transactionService.deleteAllTransactions(processId);
+        log.info("[ID: {}] Controller: All transactions deleted successfully. Returning HTTP status {}.", processId, HttpStatus.OK);
+        return ResponseEntity.ok().build();
+    }
+
+    private String generateprocessId() {
+        return UUID.randomUUID().toString();
     }
 }
