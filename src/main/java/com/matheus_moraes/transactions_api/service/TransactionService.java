e7149e89c14d1bdd37e51202fdbc75b9eebedbed
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-08-04 00:23:02 -0300

    add: slf4j

--- src/main/java/com/matheus_moraes/transactions_api/service/TransactionService.java
+++ src/main/java/com/matheus_moraes/transactions_api/service/TransactionService.java
@@ -2,6 +2,7 @@ package com.matheus_moraes.transactions_api.service;
 
 import com.matheus_moraes.transactions_api.dto.req.TransactionReqDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TransactionService {
     private final List<TransactionReqDto> transactionsList = new ArrayList<>();
 
