54b65f69aa5b34943d6b6677305121d666837be3
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-08-04 20:41:53 -0300

    refactor: process id from controller utils

--- src/main/java/com/matheus_moraes/transactions_api/controller/TransactionController.java
+++ src/main/java/com/matheus_moraes/transactions_api/controller/TransactionController.java
@@ -2,6 +2,7 @@ package com.matheus_moraes.transactions_api.controller;
 
 import com.matheus_moraes.transactions_api.dto.req.TransactionReqDto;
 import com.matheus_moraes.transactions_api.service.TransactionService;
+import com.matheus_moraes.transactions_api.util.ControllerUtils;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,18 +10,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
-
 @RestController
 @RequestMapping("/transacao")
 @RequiredArgsConstructor
 @Slf4j
 public class TransactionController {
     private final TransactionService transactionService;
+    private final ControllerUtils controllerUtils;
 
     @PostMapping
     public ResponseEntity<Void> addNewTransaction(@Valid @RequestBody TransactionReqDto request) {
-        String processId = this.generateprocessId();
+        String processId = controllerUtils.generateProcessId();
         log.info("[ID: {}] Controller: Received request to add a new transaction -> {}", processId, request);
         transactionService.addNewTransaction(request, processId);
         log.info("[ID: {}] Controller: Transaction processed successfully. Returning HTTP status {}.", processId, HttpStatus.CREATED);
@@ -29,14 +29,10 @@ public class TransactionController {
 
     @DeleteMapping
     public ResponseEntity<Void> deleteAllTransactions() {
-        String processId = this.generateprocessId();
+        String processId = controllerUtils.generateProcessId();
         log.info("[ID: {}] Controller: Received request to delete all transactions.", processId);
         transactionService.deleteAllTransactions(processId);
         log.info("[ID: {}] Controller: All transactions deleted successfully. Returning HTTP status {}.", processId, HttpStatus.OK);
         return ResponseEntity.ok().build();
-    }
-
-    private String generateprocessId() {
-        return UUID.randomUUID().toString();
     }
 }
