76d0c1f9b0033641f7fa9340ca7867e4e18ba36d
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-08-05 23:57:37 -0300

    add: documentation for transactions controller

--- src/main/java/com/matheus_moraes/transactions_api/controller/TransactionController.java
+++ src/main/java/com/matheus_moraes/transactions_api/controller/TransactionController.java
@@ -3,6 +3,9 @@ package com.matheus_moraes.transactions_api.controller;
 import com.matheus_moraes.transactions_api.dto.req.TransactionReqDto;
 import com.matheus_moraes.transactions_api.service.TransactionService;
 import com.matheus_moraes.transactions_api.util.ControllerUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +22,13 @@ public class TransactionController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(description = "Endpoint responsible for adding new transactions")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transaction added successfully"),
+            @ApiResponse(responseCode = "422", description = "Fields do not meet the transaction requirements"),
+            @ApiResponse(responseCode = "400", description = "Request error"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     public void addNewTransaction(@Valid @RequestBody TransactionReqDto request) {
         String processId = controllerUtils.generateProcessId();
         log.info("[ID: {}] Controller: Received request to add a new transaction -> {}", processId, request);
@@ -27,6 +37,11 @@ public class TransactionController {
     }
 
     @DeleteMapping
+    @Operation(description = "Endpoint responsible for deleteting all transactions")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "All transactions deleted successfully"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     public void deleteAllTransactions() {
         String processId = controllerUtils.generateProcessId();
         log.info("[ID: {}] Controller: Received request to delete all transactions.", processId);
